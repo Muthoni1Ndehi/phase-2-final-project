7f3624d107ed75af5d214832b63223439e075f7c
muthoni <muthoni.ndehi@student.moringaschool.com>
2022-10-11 14:57:44 +0300

    added fetch method function to fetch data from our API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
+import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 import AddActivity from './AddActivity';
+import NavBar from './NavBar';
+import React,{useState, useEffect} from 'react';
+import HomePage from './HomePage';
 
 function App() {
+  const[todo, setTodo]=useState([])
+  useEffect(()=>{
+    fetch('http://localhost:3000/Todo')
+    .then(ress=>ress.json())
+    .then(data=>{
+      console.log(data)
+    })
+  })
+
   return (
     <div className="App">
-      <AddActivity/>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/addtodo">
+          <AddActivity/>
+        </Route>
+        <Route exact path="/">
+          <HomePage/>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
